f41eed82b429b053458bd93a8e47c5747f2ebbc8
salty-max <max@jellycat.fr>
2023-09-17 19:47:09 +0200

    feat: draws a triangle

--- src/core/engine.ts
+++ src/core/engine.ts
@@ -7,6 +7,7 @@ import { Shader } from '../gl/shader'
 export class Engine {
   private _canvas!: HTMLCanvasElement
   private _shader!: Shader
+  private _buffer!: WebGLBuffer
 
   /**
    * Create a new engine
@@ -24,6 +25,8 @@ export class Engine {
     this.loadShaders()
     this._shader.use()
 
+    this.createBuffer()
+    this.resize()
     this.loop()
   }
 
@@ -33,11 +36,44 @@ export class Engine {
   resize() {
     this._canvas.setAttribute('width', `${window.innerWidth}`)
     this._canvas.setAttribute('height', `${window.innerHeight}`)
+
+    gl.viewport(0, 0, this._canvas.width, this._canvas.height)
   }
 
   private loop() {
     gl.clear(gl.COLOR_BUFFER_BIT)
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer)
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(0)
+    gl.drawArrays(gl.TRIANGLES, 0, 3)
+    gl.disableVertexAttribArray(0)
+    gl.bindBuffer(gl.ARRAY_BUFFER, null)
+
     requestAnimationFrame(this.loop.bind(this))
+  }
+
+  private createBuffer() {
+    const buf = gl.createBuffer()
+    if (!buf) {
+      throw new Error('Error creating buffer')
+    }
+    this._buffer = buf
+
+    // prettier-ignore
+    const vertices: number[] = [
+      // x, y, z
+      0, 0, 0,
+      0, 0.5, 0,
+      0.5, 0.5, 0,
+    ]
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer)
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(0)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+    gl.bindBuffer(gl.ARRAY_BUFFER, null)
+    gl.disableVertexAttribArray(0)
   }
 
   private loadShaders() {
