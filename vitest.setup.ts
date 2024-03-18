13ddf338168a8353b77c72e32adc9569024302b0
salty-max <max@jellycat.fr>
2024-03-18 03:04:38 +0100

    fix: declared vitest-axe types manually

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -3,5 +3,12 @@ import * as jestDOMMatchers from '@testing-library/jest-dom/matchers';
 import * as axeMatchers from 'vitest-axe/matchers';
 import { expect } from 'vitest';
 
+import type { AxeMatchers } from 'vitest-axe/matchers';
+
+declare module 'vitest' {
+  export interface Assertion extends AxeMatchers {}
+  export interface AsymmetricMatchersContaining extends AxeMatchers {}
+}
+
 expect.extend(jestDOMMatchers);
 expect.extend(axeMatchers);
